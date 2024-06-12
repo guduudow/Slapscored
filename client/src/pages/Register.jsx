52a41375a814a214ba6794ca1e80461414039ddf
guduudow <bbo3063@gmail.com>
2024-06-12 18:29:50 -0400

    fixed validation
    next step, do login and connect to userpage, also add notifications to let user know if they have registered or not.

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,9 +1,11 @@
 import "../components/Register.css";
 import axios from "axios";
 import { useForm } from "react-hook-form";
-import { Container, Col, Button, Form } from "react-bootstrap";
+import { Container, Col, Button, Form, Row } from "react-bootstrap";
+import React, { useState } from "react";
 
 function Register() {
+  const [responseMessage, setResponseMessage] = useState("");
   const {
     register,
     handleSubmit,
@@ -16,6 +18,9 @@ function Register() {
         "http://localhost:3500/auth/register",
         data
       );
+      if (response.status === 201) {
+        setResponseMessage("Account created sucessfully!");
+      }
       console.log(response.data);
     } catch (error) {
       console.error("Issue with the operation:", error);
@@ -24,45 +29,66 @@ function Register() {
 
   return (
     <>
-      <Container className="py-5">
-        <Col className="register-form form-container">
-          <Form onSubmit={handleSubmit(onSubmit)}>
-            <Form.Group className="mb-3" controlId="email">
-              <Form.Label className="label-color">Email Address</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="Enter email address"
-                {...register("email", { required: "Email is required" })}
-              />
-              {errors.email && (
-                <p className="error-message">{errors.email.message}</p>
-              )}
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="username">
-              <Form.Label className="label-color">Username</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Enter a username"
-                {...register("username", { required: "Username is required" })}
-              />
-              {errors.username && (
-                <p className="error-message">{errors.username.message}</p>
-              )}
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="password">
-              <Form.Label className="label-color">Password</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="Enter a password"
-                {...register("password", { required: "Password is required" })}
-              />
-              {errors.password && (
-                <p className="error-message">{errors.password.message}</p>
-              )}
-            </Form.Group>
-            <Button type="submit">Create Account</Button>
-          </Form>
-        </Col>
+      <Container
+        fluid
+        className="d-flex justify-content-center align-items-center min-vh-100"
+      >
+        <Row className="w-100 justify-content-center">
+          <Col className="register-form form-container" md="7" lg="4">
+            <h1 className="label-color">Register</h1>
+            <Form onSubmit={handleSubmit(onSubmit)}>
+              <Form.Group className="mb-3" controlId="email">
+                <Form.Label className="label-color">Email Address</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter email address"
+                  {...register("email", {
+                    required: "Email is required",
+                    pattern: /^[a-zA-Z0-9_.±]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/,
+                  })}
+                />
+                {errors.email && (
+                  <p className="error-message">{errors.email.message}</p>
+                )}
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="username">
+                <Form.Label className="label-color">Username</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter a username"
+                  {...register("username", {
+                    required: "Username is required",
+                  })}
+                />
+                {errors.username && (
+                  <p className="error-message">{errors.username.message}</p>
+                )}
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="password">
+                <Form.Label className="label-color">Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Enter a password"
+                  {...register("password", {
+                    required: "Password is required",
+                    minLength: {
+                      value: "10",
+                      message: "At least 10 characters.",
+                    },
+                  })}
+                />
+                {errors.password && (
+                  <p className="error-message">{errors.password.message}</p>
+                )}
+              </Form.Group>
+              <div className="d-flex justify-content-center">
+                <Button type="submit" className="create-acct">
+                  Create Account
+                </Button>
+              </div>
+            </Form>
+          </Col>
+        </Row>
       </Container>
     </>
   );
