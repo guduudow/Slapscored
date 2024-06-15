9395804090c3d352f6608945fe6a73233eefdde3
guduudow <bbo3063@gmail.com>
2024-06-15 19:59:34 -0400

    got roster page done
    clicking on team button loads roster of team. next step lock page behind user login, and create the dashboard page.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,6 +12,7 @@ import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Privacy from "./pages/Privacy";
 import Footer from "./components/Footer";
+import Roster from "./pages/Roster";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -27,6 +28,7 @@ function App() {
           <Route path="/login" Component={Login} />
           <Route path="/register" Component={Register} />
           <Route path="/dashboard" Component={Dashboard} />
+          <Route path="/roster" Component={Roster} />
         </Routes>
         <Footer />
       </Router>
